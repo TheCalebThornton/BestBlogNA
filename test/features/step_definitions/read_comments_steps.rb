8927a88a56a01a026066a536871cb38c7d15facd
Garrick <sergarrick@gmail.com>
2015-07-20 12:23:08 -0400

    Revert "ATDD"
    This reverts commit 136ac6eceb5a03c83d518cad899a1b5e94630344.

--- test/features/step_definitions/read_comments_steps.rb
+++
@@ -1,8 +0,0 @@
-require "watir-webdriver"
-
-
-Then /^I should see comments left by other readers$/ do
-  on_page PostPage do |page|
-    expect(page.commentsExist?).to be_true
-  end
-end
--- test/features/step_definitions/url_info_steps.rb
+++
@@ -1,5 +0,0 @@
-require 'rspec'
-
-Then /^the url should contain information about the post$/ do
-  expect(get_browser_url).to be eq("http://localhost:8080/grails-blog/user/post1")
-  end
